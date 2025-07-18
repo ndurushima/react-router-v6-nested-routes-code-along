99e31177bf947e27a5f838ca3d5969787e88ba52
ndurushima <ndu1994@gmail.com>
2025-07-17 19:14:54 -0500

    Create layout for all routes and fixed the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./pages/Layout"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Login from "./pages/Login"
 import UserProfile from "./pages/UserProfile"
 import ErrorPage from "./pages/ErrorPage"
-import Layout from "./pages/Layout"
 
 function App() {
   return (
     <BrowserRouter>
-
       <Routes>
-          <Route path="/" element={Layout} />
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile/:id" element={<UserProfile />} />
-          <Route path="*" element={<ErrorPage />}/>
+          <Route path="/" element={Layout}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile/:id" element={<UserProfile />} />
+            <Route path="*" element={<ErrorPage />}/>
+          </Route>
       </Routes>
     </BrowserRouter>
   )
